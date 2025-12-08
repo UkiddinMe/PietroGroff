3b195245278d858f6b5a35fc39a9a385d72c82f4
Tommaso Castagnola <tommaso.castagnola@pomiager.com>
2025-12-08 02:09:06 +0100

    Buon punto senza mobile

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // const urlPath = "https://ukiddinme.github.io/PietroGroff/"
+
+    // document.getElementsByClassName("page-content").forEach(element => {
+
+    //     if (window.location.href.endsWith(".com") && element.dataset.pageTitle === "") {
+    //         element.classList.remove("hidden");
+    //     } else if (window.location.href.split('/').last == element.dataset.pageTitle) {
+    //         element.classList.remove("hidden");
+    //     }
+
+    // });
+
+    // document.getElementsByClassName("link-to-page").addEventListener("click", (el)=>{
+
+    //     var titolo = el.dataset.linkto + "&mdash; Pietro Groff";
+    //     document.title = titolo;
+    //     window.history.pushState({"html":document.innerHTML,"pageTitle": titolo},"", urlPath);
+        
+
+    //     window.addEventListener("popstate", (e) => {
+    //         if(e.state){
+    //             document.getElementById("content").innerHTML = e.state.html;
+    //             document.title = e.state.pageTitle;
+    //         }
+    //     });
+    // })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
